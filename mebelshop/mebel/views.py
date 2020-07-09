8265084aa47f53ed9218736e447fc351dfdc0497
AlexTheMaggot <alexthemaggot23@gmail.com>
2020-07-09 09:18:53 +0500

    fix

--- mebelshop/mebel/views.py
+++ mebelshop/mebel/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Categories, Products
 
 
@@ -8,7 +9,7 @@ def index(request):
 
 def shop(request, slug_category=None):
     category = Categories.objects.all()
-    product = Products.objects.all()
+    product = Products.objects.all().order_by('-id')
 
     price_from = request.GET.get("min", "")
     price_to = request.GET.get("max", "")
@@ -17,12 +18,14 @@ def shop(request, slug_category=None):
         price_from = '0'
         price_to = '500000'
     else:
-        product = Products.objects.filter(price__gte=int(price_from), price__lte=int(price_to))
+        product = Products.objects.filter(price__gte=int(price_from), price__lte=int(price_to)).order_by('-id')
+
+    selected_category = ''
 
     if slug_category:
         category = Categories.objects.all()
         selected_category = get_object_or_404(Categories, slug_category=slug_category)
-        product = Products.objects.filter(category=selected_category)
+        product = Products.objects.filter(category=selected_category).order_by('-id')
 
         price_from = request.GET.get("min", "")
         price_to = request.GET.get("max", "")
@@ -31,13 +34,21 @@ def shop(request, slug_category=None):
             price_from = '0'
             price_to = '500000'
         else:
-            product = Products.objects.filter(category=selected_category, price__gte=int(price_from), price__lte=int(price_to))
+            product = Products.objects.filter(category=selected_category, price__gte=int(price_from), price__lte=int(price_to)).order_by('-id')
+
+
+    paginator = Paginator(product, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
         'price_from': price_from,
         'price_to': price_to,
         'category': category,
         'product': product,
+        'page_obj': page_obj,
+        'selected_category': selected_category,
     }
 
     return render(request, 'meb/shop.html', context)
